d315a8eb551d1cd22b65a7378123b1047b966e48
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-10-19 12:06:05 +0530

    completed by tabraiz

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  setInterval(() => {
+    setCount(count++)
+  }, 1000);
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        <h2>You have been on this page for {count} seconds.</h2>
+
     </div>
   )
 }
